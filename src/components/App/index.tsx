8e324d2f711b29c31eb0fbf9b8a4cc59824a6ea6
agaoldziej <agnieszka.oldziej@gmail.com>
2021-01-04 08:10:25 +0100

    Fina Minesweeper game

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -3,11 +3,16 @@ import "./App.scss";
 import NumberDisplay from '../NumberDisplay/index';
 
 const App: React.FC = () => {
+    const [cells, setCells] = useState();
 
     return (
         <div className="App">
             <div className="Header">
                 <NumberDisplay value={0} />
+                <div className="Face">
+                    <span role="img" aria-label="face">
+                    </span>
+                </div>
                 <NumberDisplay value={23} />
             </div>
             <div className="Body">Body</div>
